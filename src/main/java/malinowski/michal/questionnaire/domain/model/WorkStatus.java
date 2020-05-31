d5b419f757c37b3dd3a383a3b01f0c0a65b052f6
michaljmalinowski <michaljmalinowski@github.com>
2020-05-31 21:58:30 +0200

    create enum WorkStatus (Respondent atribute)

---
+++ src/main/java/malinowski/michal/questionnaire/domain/model/WorkStatus.java
@@ -0,0 +1,10 @@
+package malinowski.michal.questionnaire.domain.model;
+
+public enum WorkStatus {
+    BEZROBOTNY,
+    OSOBA_UCZĄCA_SIĘ,
+    PRACUJĄCY,
+    PRACUJE_I_UCZY_SIĘ,
+    EMERYT_I_PRACUJE,
+    RENCISTA_I_PRACUJE
+}
